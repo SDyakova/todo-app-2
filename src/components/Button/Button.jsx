8fc3191a9bf917081f5f847fb514b4af348652a0
Svetlana Dyakova <sdyakova@outlook.com>
2022-10-06 15:11:40 +0300

    add delete-task-functional

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import classes from './Button.module.scss';
 
-const Button = ({ type }) => {
+const Button = ({ type, onDeleted }) => {
   const btns = { edit: 'Edit', delete: 'X' };
 
-  return <button className={`${classes.btn} ${classes[type]}`}>{btns[type]}</button>;
+  return (
+    <button className={`${classes.btn} ${classes[type]}`} onClick={onDeleted}>
+      {btns[type]}
+    </button>
+  );
 };
 
 export default Button;
