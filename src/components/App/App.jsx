3b819338e3d802be9baa38e84c4ce3bd722a66e3
Svetlana Dyakova <sdyakova@outlook.com>
2022-10-06 08:59:56 +0300

    add new-task-form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from '../TaskList';
+import NewTaskForm from '../NewTaskForm';
 import classes from './App.module.scss';
 
+let maxId = 100;
+
 function App() {
+  const createTask = (text) => {
+    return {
+      id: maxId++,
+      title: text,
+      date: Date.now(),
+    };
+  };
+
+  const initialState = [createTask('New Task1'), createTask('New Task2'), createTask('New Task3')];
+  const [appState, setAppState] = useState(initialState);
+
+  const addTask = (e) => {
+    const newTask = createTask(e.target.value);
+    if (e.key === 'Enter') {
+      setAppState((appState) => {
+        return [...appState, newTask];
+      });
+      e.target.value = '';
+    }
+  };
+
   return (
     <div className={classes.App}>
       <header className={classes.header}>
         <h1 className={classes.title}>todos</h1>
       </header>
-      <section className={classes.name}></section>
-
-      <TaskList />
+      <section className={classes.main}>
+        <NewTaskForm addTask={addTask} />
+        <TaskList taskItems={appState} />
+      </section>
     </div>
   );
 }
