bece8216ad52aab741a1272311103924d87454a6
Svetlana Dyakova <sdyakova@outlook.com>
2022-10-06 15:11:54 +0300

    add delete-task-functional

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Task from '../Task';
 import classes from './TaskList.module.scss';
 
-const TaskList = ({ taskItems }) => {
+const TaskList = ({ taskItems, onDeleted }) => {
   const list = taskItems.map(({ id, title, date }) => {
-    return <Task key={id} title={title} date={date} />;
+    return <Task key={id} title={title} id={id} onDeleted={onDeleted} />;
   });
 
   return <ul className={classes.list}>{list}</ul>;
