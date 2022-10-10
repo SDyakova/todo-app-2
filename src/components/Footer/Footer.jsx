aeb477bb612685d3e204241ba48119df58e11dc4
Svetlana Dyakova <sdyakova@outlook.com>
2022-10-10 11:48:20 +0300

    add task-filter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from '../Button';
+import TaskFilter from '../TaskFilter';
 import classes from './Footer.module.scss';
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
       <span>1 items left</span>
+      <TaskFilter />
       <Button title="Clear completed" />
     </footer>
   );
--- src/components/TaskFilter/TaskFilter.jsx
+++ src/components/TaskFilter/TaskFilter.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Button from '../Button';
 import classes from './TaskFilter.module.scss';
 
 const TaskFilter = () => {
-  return <p>TaskFilter</p>;
+  const filterBtns = ['All', 'Active', 'Completed'];
+
+  const filterList = filterBtns.map((btn) => <Button title={btn} />);
+
+  return <div>{filterList}</div>;
 };
 
 export default TaskFilter;
